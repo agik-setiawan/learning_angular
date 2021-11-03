ed6ee60fe21ac8d234598988a3d4aa124d3e33ed
agik-setiawan <setiawanagik@gmail.com>
2021-11-03 22:32:59 +0700

    directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestingComponent } from './testing/testing.component';
+import { TestingDirective } from './testing.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestingComponent
+    TestingComponent,
+    TestingDirective
   ],
   imports: [
     BrowserModule,
